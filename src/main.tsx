7960933e82cc3dab190453bb926878bbef2c4e11
tahafurkanozbay-star <tahafurkan.ozbay@ogr.iu.edu.tr>
2026-09-18 14:10:40 +0300

    feat(v8): load workspace and query visual layer

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "@arcgis/core/assets/esri/themes/light/main.css";
 import "./styles/app.css";
 import "./styles/runtime.css";
 import "./styles/comfort-white.css";
+import "./styles/workspace-query.css";
 
 declare global {
   interface Window {
