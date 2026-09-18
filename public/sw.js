2b3fbb02983cafcca07f47adec788e8057b525cd
tahafurkanozbay-star <tahafurkan.ozbay@ogr.iu.edu.tr>
2026-09-18 13:09:47 +0300

    fix(v6): bump service worker cache namespace

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "altyapi-shell-v5";
+const CACHE = "altyapi-shell-v6";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./favicon.svg"];
 
 self.addEventListener("install", (event) => {
