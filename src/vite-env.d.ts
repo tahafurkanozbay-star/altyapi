e41793f67158645e428085036e0a87cf32e4ba39
tahafurkanozbay-star <tahafurkan.ozbay@ogr.iu.edu.tr>
2026-09-22 16:03:24 +0300

    feat(v14): type build provenance constants

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,6 @@
 /// <reference types="vite/client" />
 /// <reference types="@arcgis/map-components/types/react" />
+
+declare const __APP_VERSION__: string;
+declare const __BUILD_SHA__: string;
+declare const __BUILD_TIME__: string;
