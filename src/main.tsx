cd9d885507c8ef81e5ab91a51c7f685f9aae1295
tahafurkanozbay-star <tahafurkan.ozbay@ogr.iu.edu.tr>
2026-09-24 10:58:06 +0300

    style: load Ankara brand overrides

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "@arcgis/core/assets/esri/themes/light/main.css";
 import "./styles/app.css";
 import "./styles/runtime.css";
 import "./styles/comfort-white.css";
+import "./styles/ankara-brand.css";
 
 declare global {
   interface Window {
