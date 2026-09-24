bad936d77d4686300c932bc88ac7b8ced00431a6
tahafurkanozbay-star <tahafurkan.ozbay@ogr.iu.edu.tr>
2026-09-24 11:46:27 +0300

    chore: rotate PWA cache for TUCBS direct access

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const SHELL_CACHE = "altyapi-shell-v14";
-const DATA_CACHE = "altyapi-data-v14";
+const SHELL_CACHE = "altyapi-shell-v15";
+const DATA_CACHE = "altyapi-data-v15";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./favicon.svg"];
 const DATA_FILES = ["./services.json", "./service-health.json", "./service-navigation.json"];
 
