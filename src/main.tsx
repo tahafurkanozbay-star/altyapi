6b65b2d7a17db1dec77a350b7c70f5108eb27593
tahafurkanozbay-star <tahafurkan.ozbay@ogr.iu.edu.tr>
2026-09-24 11:42:51 +0300

    feat: mount TUCBS authorized access setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { AppErrorBoundary } from "./components/AppErrorBoundary";
+import { TucbsAccessSetupHost } from "./components/TucbsAccessSetup";
 import "@arcgis/core/assets/esri/themes/light/main.css";
 import "./styles/app.css";
 import "./styles/runtime.css";
@@ -27,6 +28,7 @@ window.addEventListener("unhandledrejection", (event) => {
 createRoot(root).render(
   <AppErrorBoundary>
     <App />
+    <TucbsAccessSetupHost />
   </AppErrorBoundary>
 );
 
